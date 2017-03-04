0b48c8f9579f5c7ee1b356eeb04a5f273c7a2131
Sam H <sam.hiscox@ntlworld.com>
2017-03-04 13:44:48 +0000

    Debug Averaging

--- IRS.py
+++ IRS.py
@@ -327,9 +327,9 @@ class missionRecon:
             for j in range(len(targetData[strTarget]['LAT'])):
                 if(i == j or i == 0):
                     #REMEMBER TO STRIP ZEROS
-                    #xyMatrix.append([])
-                    #xyMatrix[j].append(0)
-                    #xyMatrix[j].append(0)
+                    xyMatrix.append([])
+                    xyMatrix[j].append(0)
+                    xyMatrix[j].append(0)
                 else:
                     compLAT = targetData[strTarget]['LAT'][j]
                     compLONG = targetData[strTarget]['LONG'][j]
@@ -342,6 +342,10 @@ class missionRecon:
                         xyMatrix.append([])
                         xyMatrix[j].append(GPSestimate[0])
                         xyMatrix[j].append(GPSestimate[1])
+                    else
+                        xyMatrix.append([])
+                        xyMatrix[j].append(0)
+                        xyMatrix[j].append(0)
 
         #average GPS values, first stripping zeros
         total_lat = 0
